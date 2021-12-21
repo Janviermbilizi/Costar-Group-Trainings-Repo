bc9a37d751ad8a192273ee736c0542b4b3b3c4ab
Janvier Mbilizi <janviermbilizi@gmail.com>
2021-12-21 14:40:18 -0500

    Set and Use CORS configuration

--- DotnetCoreProject/API/Startup.cs
+++ DotnetCoreProject/API/Startup.cs
@@ -37,6 +37,9 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+
+            //Set Cors in .Net
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +55,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            //use Cors
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
