eb90c52c40e6a5bef60a65d77e44535f99c25b6c
Janvier Mbilizi <janviermbilizi@gmail.com>
2022-01-04 15:08:27 -0500

    Login API (Model, DTOs, Controller) without JWT

--- DotnetCoreProject/API/Controllers/AccountController.cs
+++ DotnetCoreProject/API/Controllers/AccountController.cs
@@ -46,5 +46,26 @@ namespace API.Controllers
         {
             return await this.context.Users.AnyAsync(user => user.Email == email.ToLower());
         }
+
+        //Login Route
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(LoginDto loginDto)
+        {   
+            var User = await this.context.Users.SingleOrDefaultAsync(user => user.Email == loginDto.Email);
+
+            if(User == null) return Unauthorized("Invalid Email, please register");
+
+            //Verifying mutch Password
+            using var hmac = new HMACSHA512(User.PasswordSalt);
+
+            var computerHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(var i = 0; i < computerHash.Length; i++)
+            {
+                if(computerHash[i] != User.PasswordHash[i]) return Unauthorized("Invalid password, please try again or reset your password");
+            }
+
+            return User;
+        }
     }
 }
